e574ce5e0c827a0a4ddeb0d448dedf14faf9d2c6
aushamim <au.shamim0@gmail.com>
2024-11-23 15:37:35 +0600

    [Update] ESlint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,20 +2,20 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintPluginPrettier from "eslint-config-prettier";
-import stylisticJs from "@stylistic/eslint-plugin-js";
+import stylistic from "@stylistic/eslint-plugin";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
   { languageOptions: { globals: globals.node } },
   { ignores: [".node_modules/*", "dist"] },
-  { plugins: { "@stylistic/js": stylisticJs } },
+  { plugins: { "@stylistic": stylistic } },
   {
     rules: {
-      "no-undef"                 : "error",
-      "prefer-const"             : "error",
-      "no-console"               : "warn",
-      "@stylistic/js/key-spacing": ["error", { align: "colon" }],
+      "no-undef"               : "error",
+      "prefer-const"           : "error",
+      "no-console"             : "warn",
+      "@stylistic/key-spacing" : ["warn", { align: { beforeColon: true } }],
     },
   },
   pluginJs.configs.recommended,
