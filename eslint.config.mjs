f2ed049ba0247b616c4ca1b624290f8ac837474b
aushamim <au.shamim0@gmail.com>
2024-11-23 15:06:38 +0600

    [Add] Eslint Align Plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,17 +2,20 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintPluginPrettier from "eslint-config-prettier";
+import stylisticJs from "@stylistic/eslint-plugin-js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
   { languageOptions: { globals: globals.node } },
   { ignores: [".node_modules/*", "dist"] },
+  { plugins: { "@stylistic/js": stylisticJs } },
   {
     rules: {
-      "no-undef": "error",
-      "prefer-const": "error",
-      "no-console": "warn",
+      "no-undef"                 : "error",
+      "prefer-const"             : "error",
+      "no-console"               : "warn",
+      "@stylistic/js/key-spacing": ["error", { align: "colon" }],
     },
   },
   pluginJs.configs.recommended,
