f97d3794783fffb9c148fce14255c58561c4da9a
aushamim <au.shamim0@gmail.com>
2025-02-27 00:22:34 +0600

    fix: 🐛 added cors origin

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ swaggerDocs(app);
 // Parsers
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
 
 // App routes
 app.use("/api", ProductRoutes);
